b1c6efd37082442be1187cced1089760ca9c4cb5
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-15 15:34:43 +0100

    map&forEach

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,88 @@ console.log(reverseWord("Reliefpfeiler"));
 console.log(reverseWord("Rentner"));
 console.log(reverseWord("Ella mag alle Bohnen"));
 console.log(reverseWord("han nesaH has ennaH"))
+
+//Lev1_1_js-vertiefung_array-iteration_forEach()
+console.log('%c---------forEach()---------', 'color:purple');
+
+let getraenke = [
+    "Coca-Cola",
+    "Apfelsaft",
+    "Pepsi",
+    "Traubensaft",
+    "Sprite",
+    "Orangensaft",
+    "Red Bull Energy Drink",
+    "Fanta"
+]
+getraenke.sort()
+console.log(getraenke)
+function myDrinks() {
+    getraenke.forEach(element => console.log(element));
+    getraenke.forEach(element => document.write(element + "<br>"));
+}
+myDrinks();
+// Lev 1:5 forEach() 
+console.log('%c---------forEach().02---------', 'color:purple');
+let checkNumber = [
+    18,
+    16,
+    80,
+    51,
+    47,
+    38,
+    95,
+    42,
+    68,
+    61,
+    34,
+    51,
+    20,
+    17,
+    56,
+    31,
+    100,
+    6,
+    5,
+    30,
+    74,
+    97,
+    28,
+    99,
+    91,
+    27,
+    73,
+    12,
+    92,
+    6,
+    27,
+    71,
+    26,
+    15,
+    78
+];
+checkNumber.forEach(e => {
+    if (e % 3 == 0) {
+        let newLocal = e + 100;
+        // return e + 100;
+        console.log(newLocal)
+    }
+    else if (e % 3 != 0) {
+        console.log(e)
+    }
+    // console.log(e)
+})
+// Lev 1-2 map() 
+console.log('%c---------map()---------', 'color:purple');
+let upperDrinks = getraenke.map(e => {
+    return e.toLocaleUpperCase();
+})
+console.log(upperDrinks);
+// Lev 1_3 Map()
+let array = [18, 16, 80, 51, 47, 38, 95, 42, 68, 61, 34, 51, 20, 17,
+    56, 31, 100, 6, 5, 30, 74, 97, 28, 99, 91, 27, 73, 12, 92, 6,
+    27, 71, 26, 15, 78
+];
+let map1 = array.map(x => x * 2);
+
+console.log(map1.sort((a, b) => a - b));
